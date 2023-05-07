2ae5f1758800491142ad0a5542293c1ec08e5bde
Veerakit SlothPete Prasertpol <71759830+slothPete7773@users.noreply.github.com>
2023-05-07 08:05:02 +0000

    temp store

--- 03-data-lake-with-google-cloud-storage/examples/uploading-to-gcs/main.py
+++ 03-data-lake-with-google-cloud-storage/examples/uploading-to-gcs/main.py
@@ -10,16 +10,19 @@ from google.oauth2 import service_account
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
+    # bucket_name = "slothpete-100033"
     # The path to your file to upload
     # source_file_name = "local/path/to/file"
     # The ID of your GCS object
     # destination_blob_name = "storage-object-name"
 
-    keyfile = os.environ.get("KEYFILE_PATH")
-    service_account_info = json.load(open(keyfile))
-    credentials = service_account.Credentials.from_service_account_info(service_account_info)
-    project_id = "dataengineercafe"
+    # keyfile = os.environ.get("KEYFILE_PATH")
+    keyfile = "data-engineer-bootcamp-384606-5c76544bd9eb-dataplex.json"
+    service_account_info = json.load(open(keyfile, "r"))
+    credentials = service_account.Credentials.from_service_account_info(
+        service_account_info
+    )
+    project_id = "data-engineer-bootcamp-384606"
 
     storage_client = storage.Client(
         project=project_id,
@@ -45,9 +48,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 
     blob.upload_from_filename(source_file_name)
 
-    print(
-        f"File {source_file_name} uploaded to {destination_blob_name}."
-    )
+    print(f"File {source_file_name} uploaded to {destination_blob_name}.")
 
 
 if __name__ == "__main__":
